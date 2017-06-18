89fa02970886171c407b1a4cb6e0231a517f272a
kuna <kgdgwn98@gmail.com>
2017-06-19 01:00:59 +0900

    rparser::util - Encoding/Decoding work for windows

--- src/util.cpp
+++ src/util.cpp
@@ -6,12 +6,106 @@
 #include <windows.h>
 #endif
 
+#ifdef USE_ICONV
+#include <iconv.h>
+#endif
 
 namespace rparser {
+
+// encoding part
+#ifdef USE_ICONV
+// use iconv
+int AttemptEncoding(std::string &s, int from_codepage)
+{
+	
+	return 0;
+}
+std::string AutoDetectEncoding(char *data, int iLen);
+std::string DecodeToUTF8(char *p, int iLen, char *encoding_from);
+std::string EncodeTo(char *p_utf8, int iLen, const std::string& to_charset);
+#else
+// use windows internal function
+int AttemptEncoding(std::string &s, int from_codepage)
+{
+	if (iLen == 0)
+	{
+		int iSize = 0;
+		iSize = AttemptEncoding(s, 949);
+		if (iSize) return iSize;
+		iSize = AttemptEncoding(s, 932);
+		if (iSize) return iSize;
+		else return 0;
+	}
+	int iSize = MultiByteToWideChar( from_codepage, MB_ERR_INVALID_CHARS, s.data(), s.size(), nullptr, 0 );
+	if( iSize == 0 ) return 0;
+	
+	// from_codepage -> UTF16
+	std::wstring sOut;
+	sOut.append( iSize, L' ' );
+	iSize = MultiByteToWideChar( from_codepage, MB_ERR_INVALID_CHARS, s.data(), s.size(), (wchar_t *) sOut.data(), iSize );
+	ASSERT( iSize != 0 );
+
+	// UTF16 -> UTF8
+	int utf8size = WideCharToMultiByte(CP_UTF8, 0, sOut.data(), sOut.size(), 0, 0, 0, 0);
+	ASSERT(utf8size);
+	s.clear();
+	s.append( utf8size, ' ' );
+	utf8size = WideCharToMultiByte(CP_UTF8, 0, sOut.data(), sOut.size(), s.data(), utf8size, 0, 0);
+	return utf8size;
+}
+std::string DecodeTo(std::string &s, int to_codepage)
+{
+	// UTF8 -> UTF16
+	int iSize = MultiByteToWideChar( CP_UTF8, MB_ERR_INVALID_CHARS, s.data(), s.size(), nullptr, 0 );
+	if( iSize == 0 ) return 0;
+	std::wstring sOut;
+	sOut.append( iSize, L' ' );
+	iSize = MultiByteToWideChar( CP_UTF8, MB_ERR_INVALID_CHARS, s.data(), s.size(), (wchar_t *) sOut.data(), iSize );
+	ASSERT( iSize != 0 );
+
+	// UTF16 -> to_codepage
+	int utf8size = WideCharToMultiByte(to_codepage, 0, sOut.data(), sOut.size(), 0, 0, 0, 0);
+	ASSERT(utf8size);
+	std::string sUTF8;
+	sUTF8.append( utf8size, ' ' );
+	utf8size = WideCharToMultiByte(to_codepage, 0, sOut.data(), sOut.size(), sUTF8.data(), utf8size, 0, 0);
+	return sUTF8;
+}
+FILE* fopen_utf8(const wchar_t* fname, const char* mode)
+{
+	return 0;
+}
+FILE* fopen_utf8(const char* fname, const char* mode)
+{
+	return 0;
+}
+std::wstring DecodeUTF8ToWStr(const std::string& s)
+{
+	return 0;
+}
+std::wstring DecodeUTF8ToWStr(char *p, int iLen)
+{
+	return 0;
+}
+std::string DecodeWStrToUTF8(const std::wstring& s)
+{
+	return 0;
+}
+int printf_utf8()
+{
+	return 0;
+}
+#endif
+
+// seed part ..?
 
 int global_seed;
 void SetSeed(int seed) { global_seed = seed; }
 int GetSeed() { return global_seed; }
+
+
+// string-path part
+
 void lower(std::string& s) {
 	std::transform(s.begin(), s.end(), s.begin(), ::tolower);
 }
--- src/util.h
+++ src/util.h
@@ -13,35 +13,39 @@
 #include <assert.h>
 #define ASSERT assert
 
+//
+// in case of linux, then USE_ICONV is automatically defined.
+// if you want to use iconv in windows, then define USE_ICONV.
+//
+#ifndef WIN32
+#define USE_ICONV
+#endif
+
 namespace rparser {
 
+// encoding related
+
 // @description
-// actually, it only detects Shift_JIS / CP949 / UTF-8 encodings.
-std::string AutoDetectEncoding(char *data, int iLen);
-std::string DecodeToUTF8(char *p, int iLen, char *encoding_from);
-// @description we may need this function in case of saving BMS in Shift_JIS format.
-std::string EncodeToJIS(char *p_utf8, int iLen);
-
-
-#ifdef WIN32
+// this returns UTF-8 string using specified encoding.
+// if no encoding found then it returns 0 with no string changed.
+// if from_codepage=0, then attempt encoding Shift_JIS / CP949
+int AttemptEncoding(std::string &s, int from_codepage=0);
+std::string DecodeTo(std::string &s, int to_codepage);
+#ifndef USE_ICONV	//#ifdef WIN32
 FILE* fopen_utf8(const wchar_t* fname, const char* mode);
 FILE* fopen_utf8(const char* fname, const char* mode);
 std::wstring DecodeUTF8ToWStr(const std::string& s);
 std::wstring DecodeUTF8ToWStr(char *p, int iLen);
 std::string DecodeWStrToUTF8(const std::wstring& s);
-#else
-#define fopen fopen_utf8
-#endif
-
-
-
-#ifdef WIN32
-// @description
-// just for debugging, to see some information.
 int printf_utf8();
 #else
+#define fopen fopen_utf8
 #define printf printf_utf8
 #endif
+
+
+
+// seed related (depreciated)
 
 // @description
 // set global seed (mostly used for BMS parsing)
@@ -50,6 +54,9 @@ void SetSeed(int seed);
 // get seed
 int GetSeed();
 
+
+
+// string related
 
 void lower(std::string& s);
 bool EndsWith(const std::string& s1, const std::string& s2);
@@ -63,36 +70,15 @@ std::string GetFilename(const std::string& path);
 std::string GetExtension(const std::string& path);
 bool IsDirectory(const std::string& path);
 
+
+
+// BMS-util related
+
 // @description only change 2 character; 00-ZZ to integer
 int atoi_bms(const char* p);
 // 00-FF
 int atoi_bms16(const char* p);
 
-
-// @description
-// simple library for memory stream, able to integrating with file stream
-class MemoryInputStream : public std::istream
-{
-public:
-	MemoryInputStream(const uint8_t* aData, size_t aLength) :
-		std::istream(&m_buffer),
-		m_buffer(aData, aLength)
-	{
-		rdbuf(&m_buffer); // reset the buffer after it has been properly constructed
-	}
-
-private:
-	class MemoryBuffer : public std::basic_streambuf<char>
-	{
-	public:
-		MemoryBuffer(const uint8_t* aData, size_t aLength)
-		{
-			setg((char*)aData, (char*)aData, (char*)aData + aLength);
-		}
-	};
-
-	MemoryBuffer m_buffer;
-};
 
 };
 
