8226d62e0c43dc12c8d01a663b15910098474760
kuna <kgdgwn98@gmail.com>
2019-09-26 23:28:38 +0900

    Error more tidy

--- src/Error.h
+++ src/Error.h
@@ -1,26 +1,26 @@
 #ifndef RPARSER_ERROR_H
 #define RPARSER_ERROR_H
 
-#define ERR(name,msg) name
 namespace rparser {
 
 enum class ERROR
 {
+#define ERR(name,msg) name
 #include "Error.list"
-};
 #undef ERR
+};
 
-#define ERR(name,msg) msg
 static const char* get_error_msg(ERROR e)
 {
 	static const char *ERROR_MSG[] =
   {
+#define ERR(name,msg) msg
 #include "Error.list"
+#undef ERR
   };
 	return ERROR_MSG[(unsigned)e];
 }
 
 };
-#undef ERR
 
 #endif
